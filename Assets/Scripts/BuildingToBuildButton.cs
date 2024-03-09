b58ac6db954891c294eb558ec7c168dbce49dbb6
Bartosz Stochel <bhq@op.pl>
2024-03-09 12:20:01 +0100

    Mamy ceny budynków

--- Assets/Scripts/BuildingToBuildButton.cs
+++ Assets/Scripts/BuildingToBuildButton.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,10 +6,17 @@ public class BuildingToBuildButton : MonoBehaviour
 {
 	[SerializeField] private Image sprite;
 	[SerializeField] private TextMeshProUGUI buildingName;
+	[SerializeField] private TextMeshProUGUI moneyCost;
+	[SerializeField] private TextMeshProUGUI woodCost;
 
 	public void Initialize(BuildingData data)
 	{
 		sprite.sprite = data.Sprite;
 		buildingName.text = data.NameToDisplay;
+		moneyCost.text = data.MoneyCost.ToString();
+		woodCost.text = data.WoodCost.ToString();
+
+		moneyCost.gameObject.SetActive(data.MoneyCost > 0);
+		woodCost.gameObject.SetActive(data.WoodCost > 0);
 	}
 }
