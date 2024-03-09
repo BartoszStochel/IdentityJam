12274b48539469e923c2b13d71bdea68e06b30db
Bartosz Stochel <bhq@op.pl>
2024-03-09 12:03:56 +0100

    Widzimy zasoby i budynki

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] private Field fieldPrefab;
-	[SerializeField] private GameObject fieldsContainer;
+	[SerializeField] private Transform fieldsContainer;
 	[SerializeField] private int mapSizeX;
 	[SerializeField] private int mapSizeY;
 	[SerializeField] private int spacingX;
 	[SerializeField] private int spacingY;
 
+	[SerializeField] private Transform buildingsToBuildContainer;
+	[SerializeField] private BuildingToBuildButton buildingToBuildPrefab;
+	[SerializeField] private List<BuildingData> buildingDatas;
+
+	[SerializeField] private TextMeshProUGUI cashLabel;
+	[SerializeField] private TextMeshProUGUI crudeLabel;
+	[SerializeField] private TextMeshProUGUI woodLabel;
+
+	[SerializeField] private int startingCash;
+
+	private int currentCash;
+	private int currentCrude;
+	private int currentWood;
+
 	private Field[,] fields;
 
 	private void Start()
+	{
+		GenerateFields();
+		GenerateBuildingsToBuild();
+
+		currentCash = startingCash;
+
+		UpdateResourcesLabels();
+	}
+
+	private void GenerateFields()
 	{
 		fields = new Field[mapSizeX, mapSizeY];
 
@@ -21,12 +46,29 @@ public class GameManager : MonoBehaviour
 		{
 			for (int y = 0; y < mapSizeY; y++)
 			{
-				var field = Instantiate(fieldPrefab, fieldsContainer.transform);
+				var field = Instantiate(fieldPrefab, fieldsContainer);
 				var rectTransform = field.GetComponent<RectTransform>();
 				rectTransform.anchoredPosition = new Vector2(x * spacingX - (mapSizeX - 1) * spacingX / 2f, y * spacingY - (mapSizeY - 1) * spacingY / 2f);
 				field.name = $"Field {x}, {y}";
 				fields[x, y] = field;
 			}
 		}
+	}
+
+	private void GenerateBuildingsToBuild()
+	{
+		foreach (var data in buildingDatas)
+		{
+			var buildingButton = Instantiate(buildingToBuildPrefab, buildingsToBuildContainer);
+			buildingButton.Initialize(data);
+			buildingButton.name = data.name + "Button";
+		}
+	}
+
+	private void UpdateResourcesLabels()
+	{
+		cashLabel.text = currentCash.ToString();
+		crudeLabel.text = currentCrude.ToString();
+		woodLabel.text = currentWood.ToString();
 	}
 }
